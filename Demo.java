5fe91da47842fd80da7f4768dbb1308d568e394d
roxdhit <rohitprasad5390@gmail.com>
2025-01-28 01:10:43 +0530

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,16 @@
+package Strings;
+import java.util.*;
+public class Demo {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter your name");
+		String name = sc.nextLine();
+		System.out.println("College name");
+		String col = sc.nextLine();
+		System.out.println("Enter your percentage");
+		float per = sc.nextFloat();
+		System.out.println("Enter your branch");
+		String branch = sc.next();
+}
+}
+	
