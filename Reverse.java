0766c32703ac6d092c5b1dc6090930d8d2b05e25
roxdhit <rohitprasad5390@gmail.com>
2025-01-28 01:11:15 +0530

    Add files via upload

--- Reverse.java
+++ Reverse.java
@@ -1,19 +1,25 @@
-package Recursion;
+package Strings;
 
 public class Reverse {
-	public static int rec_rev(int num, int res){
-		if(num == 0) {
-			return res;
-		}
-		return rec_rev(num/10, (res*10) + num % 10);
-	}
 
 	public static void main(String[] args) {
-		int num = 256;
-		int res =0;
-		System.out.println(rec_rev(num, res));
-		
+		// TODO Auto-generated method stub
+		//from last to first
+				String s = "420";
+				String res = "";
+				for(int i = s.length()-1;i>=0;i--) {
+					res = res+s.charAt(i);
+				}
+				System.out.println(res);
+				
+				//from first to last
+				String s1 = "1000";
+				String res1 = "";
+				for(int i =0; i<s1.length();i++) {
+					res1 = s1.charAt(i) + res1;
+				}
+				System.out.println(res1);
 
-	}
+			}
 
 }
