d02806fe3d0719610a30af0e1fa35fe2aa83f84e
roxdhit <rohitprasad5390@gmail.com>
2025-01-28 01:12:02 +0530

    Add files via upload

---
+++ ternary.java
@@ -0,0 +1,16 @@
+package Operators;
+
+public class ternary {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 20;
+        int b= 30;
+        int c =(a>b)?a:b;
+        System.out.println(c);
+
+	}
+
+}
+	
+
