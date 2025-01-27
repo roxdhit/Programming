26e630cafdddb57210dd024b2ba2f1b4c3bea3f3
roxdhit <rohitprasad5390@gmail.com>
2025-01-28 01:04:49 +0530

    Add files via upload

---
+++ Descending_BS.java
@@ -0,0 +1,30 @@
+package BinarySearch;
+
+public class Descending_BS {
+	public static int binary_search(int[] arr, int key) {
+		int low =0;
+		int high = arr.length-1;
+		while(low <= high) {
+			int mid = (low+high)/2;
+			if(arr[mid] == key) {
+				return mid;
+			}
+			else if(key > arr[mid]) {
+				low = mid + 1;
+			}
+			else {
+				high = mid-1;
+			}
+		}
+		return -1;
+	}
+
+
+public static void main(String [] args) {
+	int[] arr = {12, 23, 43, 54, 46, 657, 546};
+	int key = 46;
+	binary_search(arr, key);
+	
+}
+}
+	
